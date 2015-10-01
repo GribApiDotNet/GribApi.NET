a337dbb3a7aa77aab7f13442a909c55c28ca4e97
Eric Millin <ericmillin@awhere.com>
2015-10-01 10:42:43 -0400

    Added grib_api_version.h

---
+++ ext/grib_api-1.14.0-Source/src/grib_api_version.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2005-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+ * virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+ */
+
+#ifndef grib_api_version_H
+#define grib_api_version_H
+
+#define GRIB_API_VERSION_STR      "1.14.0"
+
+#define GRIB_API_MAJOR_VERSION    1
+#define GRIB_API_MINOR_VERSION    14
+#define GRIB_API_REVISION_VERSION 0
+#define GRIB_API_VERSION (GRIB_API_MAJOR_VERSION*10000+GRIB_API_MINOR_VERSION*100+GRIB_API_REVISION_VERSION)
+
+#endif /* grib_api_version_H */
