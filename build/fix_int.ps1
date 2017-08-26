80dc4685cf73e9ee6c4d35008b048ef5f2f056fa
0x1mason <etmillin@gmail.com>
2017-08-26 16:31:58 -0400

    clean build

--- build/fix_int.ps1
+++
@@ -1,5 +0,0 @@
-$path="..\src\GribApi.NET\Grib.Api\Interop\SWIG\GribApiProxy.cs"
-
-(Get-Content $path) | 
-Foreach-Object {$_ -replace "public static readonly void GRIB", "public static readonly int GRIB"} | 
-Set-Content $path
--- build/swig_gen.cmd
+++
@@ -1,8 +0,0 @@
-swig -csharp -c++ -dllimport Grib.Api.Native.dll -o Grib_Api_Native.cpp -namespace Grib.Api.Interop.SWIG -outdir %~dp0..\src\GribApi.NET\Grib.Api\Interop\SWIG -DSWIG_CSHARP_NO_IMCLASS_STATIC_CONSTRUCTOR -I%~dp0..\ext\grib_api\src grib_api.i
-
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-move %~dp0Grib_Api_Native.cpp %~dp0..\src\GribApi.NET\Grib.Api.Native\Grib_Api_Native.cpp
-
-powershell -ExecutionPolicy Unrestricted -File %~dp0fix_int.ps1
-
